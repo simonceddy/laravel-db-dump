4821c771aa8f595f89ce287ba0a683637a2484e2
Simon Eddy <simoneddy@Simons-MacBook-Pro.local>
2019-08-07 13:44:36 +1000

    added functionality

--- src/DbDumpCommand.php
+++ src/DbDumpCommand.php
@@ -2,6 +2,10 @@
 namespace Eddy\DbDump;
 
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Config\Repository as Config;
+use Illuminate\Filesystem\Filesystem;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class DbDumpCommand extends Command
 {
@@ -9,14 +13,51 @@ class DbDumpCommand extends Command
 
     protected $description = 'Dump the database as an SQL file.';
 
-    public function __construct()
+    private $config;
+
+    private $fs;
+
+    public function __construct(Config $config, Filesystem $fs)
     {
+        $this->config = $config;
+        $this->fs = $fs;
         parent::__construct();
     }
 
     public function handle()
     {
-        $this->alert('testing');
-        dd($this->laravel);
+        if ('mysql' !== $this->config->get('database.default')) {
+            $this->error('This command currently supports only MySQL');
+            return;
+        }
+
+        $conn = $this->config->get('database.connections.mysql');
+
+        $fn = $conn['database'] . '_' . Carbon::now()->getTimestamp() . '.sql';
+
+        if (!$this->fs->exists('storage/backup')) {
+            $this->info('Backup directory not found. Creating backup directory in storage/backup...');
+            
+            $this->fs->makeDirectory('storage/backup');
+            
+            $this->info('Adding .gitignore to storage/backup...');
+            
+            $this->fs->put('storage/backup/.gitignore', "*.sql\n");
+        }
+
+        $process = new Process(sprintf(
+            'mysqldump -u %s -p%s %s > %s',
+            $conn['username'],
+            $conn['password'],
+            $conn['database'],
+            $path = 'storage/backup/' . $fn
+        ));
+
+        try {
+            $process->mustRun();
+            $this->info('Database exported successfully to '.$path);
+        } catch (ProcessFailedException $e) {
+            throw $e;
+        }
     }
 }
